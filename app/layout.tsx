33849aeba2d35aea932a60954f0b16485810b27f
dhimiterPoison <dhimiter.helmi@gmail.com>
2023-07-25 18:41:39 +0200

    homepage layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Montserrat } from 'next/font/google';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { Analytics } from '@vercel/analytics/react';
 import Logo from './components/Logo';
 
 const inter = Inter({ subsets: ['latin'] });
+
+const monserrat = Montserrat({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: 'Dhimiter Helmi Portfolio',
@@ -20,17 +23,15 @@ export default function RootLayout({
 }) {
     return (
         <html lang='en'>
-            <body className={inter.className}>
+            <body className={monserrat.className}>
                 <div className='app flex overflow-hidden flex-col justify-start md:flex-row'>
                     <div className='navigation py-2 md:py-10 flex flex-col'>
                         <Logo />
                         <Navbar />
                     </div>
-                    {/* <div className='py-10   px-3 md:pl-0 md:pr-10'> */}
-                        <div className='content w-full flex rounded-t-2xl md:rounded-lg bg-base-300 p-4 justify-center my-4 md:my-10 md:mr-10'>
-                            {children}
-                        </div>
-                    {/* </div> */}
+                    <div className='content w-full flex rounded-t-2xl md:rounded-lg bg-base-300 p-4 md:p-10 justify-center my-4 md:my-6 md:mr-6'>
+                        {children}
+                    </div>
                 </div>
                 <Footer />
                 <Analytics />
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,48 @@
-import Image from 'next/image';
+import { Montserrat } from 'next/font/google';
+import MyCarousel from './components/MyCarousel';
+
+const monserrat = Montserrat({ subsets: ['latin'] });
 
 export default function Home() {
     return (
-        <main className='flex flex-col my-10 justify-start'>
+        <main
+            className={`flex flex-col my-4 justify-start ${monserrat.className}`}
+        >
+            <div className='flex flex-row justify-center text-3xl font-bold'>
+                unleash Powerful
+            </div>
             <div className='flex flex-row justify-center text-3xl font-bold'>
                 {/* //set fist letter to secondary color */}
                 <span className='text-primary'>U</span>ser e
                 <span className='text-secondary'>X</span>perience
             </div>
-            <div className='flex flex-row justify-center text-3xl font-bold'>
-                consultant
+            <div className='flex flex-row justify-center text-xl h-16 items-center'>
+                digital & analog
             </div>
-            <div className='flex flex-row justify-center text-3xl font-bold mt-32'>
-                protfolio / WiP
+            <div className='flex flex-row justify-center text-xl h-12 items-center'>
+                <div className='flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-gray-700 bg-lime-100 '>
+                    <div className='text-xs font-normal leading-none max-w-full flex-initial'>
+                        Listen
+                    </div>
+                </div>
+                <div className='flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-gray-700 bg-lime-200 '>
+                    <div className='text-xs font-normal leading-none max-w-full flex-initial'>
+                        Change perspective
+                    </div>
+                </div>
+                <div className='flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-gray-700 bg-lime-100 '>
+                    <div className='text-xs font-normal leading-none max-w-full flex-initial'>
+                        Reset limits
+                    </div>
+                </div>
+                <div className='flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full text-gray-700 bg-lime-200 '>
+                    <div className='text-xs font-normal leading-none max-w-full flex-initial'>
+                        Feel
+                    </div>
+                </div>
+            </div>
+            <div className='flex flex-row justify-center text-3xl font-bold h-full items-center max-w-full'>
+                <MyCarousel /> 
             </div>
         </main>
     );
