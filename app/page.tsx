dbc8969ff8d707688e51da8caf442f2a7b32875c
dhimiterPoison <dhimiter.helmi@gmail.com>
2023-07-25 13:08:02 +0200

    homepage test

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,18 @@ import Image from 'next/image';
 
 export default function Home() {
     return (
-        <main className='flex flex-col items-center justify-between'>
-            <div className="flex flex-row text-3xl font-bold">TEST PROJECT /home</div>
+        <main className='flex flex-col my-10 justify-start'>
+            <div className='flex flex-row justify-center text-3xl font-bold'>
+                {/* //set fist letter to secondary color */}
+                <span className='text-primary'>U</span>ser e
+                <span className='text-secondary'>X</span>perience
+            </div>
+            <div className='flex flex-row justify-center text-3xl font-bold'>
+                consultant
+            </div>
+            <div className='flex flex-row justify-center text-3xl font-bold mt-32'>
+                protfolio / WiP
+            </div>
         </main>
     );
 }
