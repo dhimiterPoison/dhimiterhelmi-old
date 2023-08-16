adae78376a5f6da1999c85eb45139f0dd9b08e63
dhimiterPoison <dhimiter.helmi@gmail.com>
2023-08-16 16:10:04 +0200

    altre piccole migliorie

--- app/components/ShortPresentation.tsx
+++ app/components/ShortPresentation.tsx
@@ -58,7 +58,7 @@ const ShortPresentation = () => {
 								xmlns='http://www.w3.org/2000/svg'
 								fill='none'
 								viewBox='0 0 24 24'
-								strokeWidth={2.5}
+								strokeWidth={2}
 								stroke='currentColor'
 								className='w-6 h-6'
 							>
