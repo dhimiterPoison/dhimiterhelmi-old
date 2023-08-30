448436d8eeefec4b5017ed0bbefb10411950f4c5
dhimiterPoison <dhimiter.helmi@gmail.com>
2023-08-30 08:10:21 +0200

    modiifiche

--- app/components/MainHero.tsx
+++ app/components/MainHero.tsx
@@ -3,14 +3,19 @@ import React from 'react';
 const MainHero = () => {
 	return (
 		<div className='flex flex-col mb-4'>
-			<div className='flex flex-row justify-center text-2xl md:text-5xl font-bold'>
+			<div className='flex flex-row justify-center text-3xl md:text-5xl font-bold'>
 				unleash Powerful
 			</div>
-			<div className='flex flex-row justify-center text-2xl md:text-5xl font-bold'>
+			<div className='flex flex-row justify-center text-3xl md:text-5xl font-bold'>
 				{/* //set fist letter to secondary color */}
 				<span className='text-primary'>U</span>ser e
 				<span className='text-secondary'>X</span>perience
 			</div>
+			{/* <div className='flex flex-col items-center my-4 prose'>
+				Whatever you do, <br /> isn’t it better enjoying doing it? <br /> I help you by
+				providing product or services tailored to your needs. <br /> Win win, I create
+				and have fun, <br /> you save time and get a smile.
+			</div> */}
 			<div className='flex flex-row justify-center text-xl md:text-2xl h-12 pt-2 md:h-16 md:items-center md:pt-0 md:pb-4'>
 				digital & analog
 			</div>
