03bbcca9fb65e1b27fc2fa21b95567e29a81a7f7
dhimiterPoison <dhimiter.helmi@gmail.com>
2023-09-07 19:35:12 +0200

    let's inroduce a blog ;)

---
+++ app/blog/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogHomepage = () => {
+  return (
+	<div className='flex flex-col gap-8 w-full min-h-full items-center justify-center prose'>
+		<h1>Dhim Blog</h1>
+		<h3>Work in progress!</h3>
+	</div>
+  )
+}
+
+export default BlogHomepage
